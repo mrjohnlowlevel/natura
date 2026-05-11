591e86f772378979e6692d0daacea134dde634df
mrjohnlowlevel <johnfree751@gmail.com>
2026-05-11 20:36:48 +0800

    improves info

--- src/lib/Info.svelte
+++ src/lib/Info.svelte
@@ -9,19 +9,33 @@
                 Natura.
             </p>
         </div>
-        <div class="md:justify-self-end md:mt-0 mt-6 justify-self-center pr-12">
+        <div class="md:justify-self-end md:mt-0 mt-6 text-center pr-12">
             <img
                 src="/natura/icon.jpeg"
                 alt="Natura"
                 height="200px"
                 width="200px"
-                class="rounded-2xl shadow-md"
+                class="rounded-2xl shadow-md hover:scale-105 transition-transform duration-200"
             />
         </div>
     </div>
-    <div id="learn-more" class="bg-white p-6">
-        <h1 class="font-bold text-4xl font-roboto text-shadow-lg">
+    <div id="learn-more" class="p-6 bg-white">
+        <h1 class="font-bold text-4xl font-roboto text-center text-shadow-lg mb-2">
             Natural Disasters
         </h1>
+        <div class="text-center grid grid-cols-1 md:grid-cols-2 justify-items-center bg-[#edf2fb] p-12 gap-2.5 rounded-sm">
+            <div class="bg-white size-40 flex items-center justify-center shadow-sm rounded-2xl">
+                <h1>Earthquake</h1>
+            </div>
+            <div class="bg-white size-40 flex items-center justify-center shadow-sm rounded-2xl">
+                <h1>Hurricane</h1>
+            </div>
+            <div class="bg-white size-40 flex items-center justify-center shadow-sm rounded-2xl">
+                <h1>Landslide</h1>
+            </div>
+            <div class="bg-white size-40 flex items-center justify-center shadow-sm rounded-2xl">
+                <h1>Tsunami</h1>
+            </div>
+        </div>
     </div>
 </div>
