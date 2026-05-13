cabb062dcc4692cc1c81479d5327950b390f4f72
mrjohnlowlevel <johnfree751@gmail.com>
2026-05-13 18:58:36 +0800

    mess cleaned + new mess

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,7 +3,7 @@
         <a href="/natura/" class="align-middle col-span-4">
             <img src="/natura/icon.jpeg" alt="Natura icon" height="80px" width="80px" />
         </a>
-        <a class="font-bold font-mono" href="#learn-more"><p>Learn About Natural Disasters</p></a>
+        <a class="font-bold font-mono" href="#learn-more"><p>Learn</p></a>
         <a class="font-bold font-mono" href="#sim"><p>Simulator</p></a>
     </div>
     <br>
--- src/lib/Info.svelte
+++ src/lib/Info.svelte
@@ -1,3 +1,28 @@
+<script lang="ts">
+    const disasters = [
+        {
+            title: "Earthquake",
+            description: "Earthquake is the sudden shaking of the Earth surface caused by the rapid release of energy in the Earth outer layer, called lithospere. This energy creates seismic waves that travel through the ground.",
+            bullets: ["Sudden ground shaking", "Tectonic plate shifts", "Measured by seismographs"],
+        },
+        {
+            title: "Hurricane",
+            description: "Powerful rotating winds Forms over warm oceans Heavy rain and flooding",
+            bullets: ["Powerful rotating winds", "Forms over warm oceans", "Heavy rain and flooding"],
+        },
+        {
+            title: "Landslide",
+            description: "Rapid rock/debris flow Triggered by heavy rain Common on steep slopes",
+            bullets: ["Rapid rock/debris flow", "Triggered by heavy rain", "Common on steep slopes"]
+        },
+        {
+            title: "Tsunami",
+            description: "Large underwater waves Caused by earthquakes High coastal impact",
+            bullets: ["Large underwater waves", "Caused by earthquakes", "High coastal impact"],
+        }
+    ]
+</script>
+
 <div>
     <div class="mb-60 pl-6 grid grid-cols-1 md:grid-cols-2">
         <div class="sm:text-center">
@@ -20,22 +45,23 @@
         </div>
     </div>
     <div id="learn-more" class="p-6 bg-white">
-        <h1 class="font-bold text-4xl font-roboto text-center text-shadow-lg mb-2">
+        <h1
+            class="font-bold text-4xl font-roboto text-center text-shadow-lg mb-2"
+        >
             Natural Disasters
         </h1>
         <div class="text-center grid grid-cols-1 md:grid-cols-2 justify-items-center bg-[#edf2fb] p-12 gap-2.5 rounded-sm">
-            <div class="bg-white size-40 flex items-center justify-center shadow-sm rounded-2xl">
-                <h1>Earthquake</h1>
-            </div>
-            <div class="bg-white size-40 flex items-center justify-center shadow-sm rounded-2xl">
-                <h1>Hurricane</h1>
-            </div>
-            <div class="bg-white size-40 flex items-center justify-center shadow-sm rounded-2xl">
-                <h1>Landslide</h1>
-            </div>
-            <div class="bg-white size-40 flex items-center justify-center shadow-sm rounded-2xl">
-                <h1>Tsunami</h1>
-            </div>
+            {#each disasters as disaster }
+                <div class="bg-white size-fit grid grid-cols-1 items-center justify-center shadow-sm rounded-2xl">
+                    <h1 class="mb-5 bold text-2xl">{ disaster.title }</h1>
+                    <p class="mb-5 p-6">{ disaster.description }</p>
+                    <ul>
+                        {#each disaster.bullets as point}
+                            <li>・ { point }</li>
+                        {/each}
+                    </ul>
+                </div>
+            {/each}
         </div>
     </div>
 </div>
