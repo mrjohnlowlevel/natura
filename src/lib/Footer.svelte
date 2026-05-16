e82d27c2597bbbf26943999b9f49d51e5c2e4789
mrjohnlowlevel <johnfree751@gmail.com>
2026-05-16 23:02:21 +0800

    hamburg menu

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,5 @@
+<div class="bg-green-600 p-3 rounded-t"></div>
+<div class="bg-[#2b5300] p-4 text-white">
+    <p>Made With Svelte</p>
+    <a href="https://github.com/goldikova/natura" class="hover:text-black transition-colors duration-200"><span>Github: https://github.com/goldikova/natura</span></a>
+</div>
